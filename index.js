42d40696937e6132ddf1dd4b22f83d1b87a2c5da
Vedant Chainani <65389981+Envoy-VC@users.noreply.github.com>
2021-02-01 01:28:58 -0800

    Update index.js

--- index.js
+++ index.js
@@ -12,12 +12,19 @@ async function main() {
         await fetch("http://quotes.stormconsultancy.co.uk/random.json")
     ).json();
 
+    const get_joke = await (
+        await fetch("https://official-joke-api.appspot.com/jokes/programming/random")
+    ).json();
+
     console.log(quote);
-    
+    console.log(get_joke);
+
 
     const readme = readmeTemplate
         .replace("{quote}", quote.quote)
         .replace("{author}", quote.author)
+        .replace("{joke}",get_joke.setup)
+        .replace("{punchline}",get_joke.punchline)
 
     await fs.writeFile("README.md", readme);
 }
